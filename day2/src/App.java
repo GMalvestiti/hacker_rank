c09c34400eac125d457f7f1a8b8e07c3d05c2486
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-07-16 17:41:44 -0300

    day 2 challenge

--- day2/src/App.java
+++ day2/src/App.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void solve(double mealCost, int tipPercent, int taxPercent) {
+        // Total cost calculation
+        Double tip = (tipPercent * mealCost) / 100;
+        Double tax = (taxPercent * mealCost) / 100;
+        int totalCost = (int) Math.round(mealCost + tip + tax);
+        System.out.println(totalCost);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double mealCost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tipPercent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int taxPercent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        App.solve(mealCost, tipPercent, taxPercent);
+
+        bufferedReader.close();
     }
 }
