9554083052d6b818298d8debc7e39986837f1215
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-07-17 17:33:39 -0300

    day 3 challenge

--- day3/src/App.java
+++ day3/src/App.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
