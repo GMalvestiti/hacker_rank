faa8475a1d198aff1c70b9d19134b0012020f2b5
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-07-17 17:31:17 -0300

    day 3 challenge

---
+++ day3/src/App.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        if (((N % 2) != 0) || ((N >= 6) && (N <= 20))) {
+            System.out.println("Weird");
+        } else {
+            System.out.println("Not Weird");
+        }
+
+        bufferedReader.close();
+    }
+}
