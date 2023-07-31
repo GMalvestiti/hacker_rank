b0c33c2b0138b8be7368fff389a397fee0d94dff
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-07-30 23:04:21 -0300

    dat 17 challenge

--- java30/day17/src/App.java
+++ java30/day17/src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Calculator {
 
-    int power(int a, int b) throws Exception {
+    public int power(int a, int b) throws Exception {
         if (a < 0 || b < 0) {
             throw new Exception("n and p should be non-negative");
         } else {
